cef2ec53fb886e53bad16383fa5256c082ee2815
Mubashir <mubashirnafiu@gmail.com>
2026-06-09 18:01:12 +0100

    working on the images

--- sanity.config.ts
+++ sanity.config.ts
@@ -18,7 +18,10 @@ export default defineConfig({
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
-  schemaTypes,
+  // plugins: [structureTool()],
+  schema: {
+    types: schemaTypes, // ← must be here
+  },
   plugins: [
     structureTool({structure}),
     // Vision is for querying with GROQ from inside the Studio
