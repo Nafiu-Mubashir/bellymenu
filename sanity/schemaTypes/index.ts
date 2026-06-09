2313cfe3e73b76a26bf64345fcc71ac7128a53c6
Mubashir <mubashirnafiu@gmail.com>
2026-06-09 17:45:04 +0100

    updated sanity details

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,17 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { homepageSchema } from "./homepage";
+import { serviceSchema } from "./service";
+import { testimonialSchema } from "./testimonial";
+import { galleryItemSchema } from "./galleryItem";
+import { aboutSchema } from "./about";
+import { menuCategorySchema } from "./menuCategory";
+import { faqSchema } from "./faq";
 
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+export const schemaTypes = [
+  homepageSchema,
+  serviceSchema,
+  testimonialSchema,
+  galleryItemSchema,
+   aboutSchema,
+   menuCategorySchema,
+   faqSchema,
+];
